2c31a3c2a06de428831cfd6f667d4f1137413b04
tiagoferrari-projects <tiago-projects@outlook.com>
2022-01-23 17:57:05 -0300

    adicionado metodos get, set e custo para encher o tanque na classe Carro

--- ProgramacaoOrientadaObjeto/src/Carro.java
+++ ProgramacaoOrientadaObjeto/src/Carro.java
@@ -3,4 +3,43 @@ public class Carro {
     String cor;
     String modelo;
     int tanqueCombustivel;
+
+    public Carro(){
+
+    }
+
+    public Carro(String cor, String modelo, int tanqueCombustivel) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.tanqueCombustivel = tanqueCombustivel;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getTanqueCombustivel(){
+        return tanqueCombustivel;
+    }
+
+    public void setTanqueCombustivel(int tanqueCombustivel){
+        this.tanqueCombustivel = tanqueCombustivel;
+    }
+
+    public double valorEncherTanque (double valorCombustivel){
+        return valorCombustivel * tanqueCombustivel;
+    }
+
 }
