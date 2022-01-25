d964b2fd67cdb02d04801ff2f4bd97c2da2c8c0a
tiagoferrari-projects <tiago-projects@outlook.com>
2022-01-25 20:27:08 -0300

    adicionado novo pacote polimorfismo e exercicios

---
+++ ProgramacaoOrientadaObjeto/src/com/github/tiagoferrari/polimorfismo/exercicio/ClasseFilha2.java
@@ -0,0 +1,14 @@
+package com.github.tiagoferrari.polimorfismo.exercicio;
+
+public class ClasseFilha2 extends ClasseMae{
+
+    @Override
+    void metodo1 (){
+        System.out.println("Método 1 da Classe Filha 2");
+    }
+
+    @Override
+    void metodo2 (){
+        System.out.println("Método 2 da Classe Filha 2");
+    }
+}
